1530ad4bd14d94d048b08dd3743e42aef551a41b
Jeevan <jeevanaher732@gmail.com>
2026-01-04 17:56:08 -0600

    binary Search added

---
+++ Binary_search.py
@@ -0,0 +1,36 @@
+def binary_search(nums,target):
+
+    n=len(nums)
+    low=0
+    high=n-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1    
+
+nums=[1,2,3,4,5,6,7,8,9]
+target=3
+result=binary_search(nums,target)
+print("Element found at index:",result+1)
+
+
+#now We wille be using the Recursive version of it
+def recursive_binary_search(nums,tager,low,high):
+    mid=(low+high)//2
+    if mid==target:
+        return mid
+    elif low>high:
+        return -1
+    if mid>target:
+        return recursive_binary_search(nums,target,low,mid-1)
+    else:
+        return recursive_binary_search(nums,target,mid+1,high)    
+    
+
+result_recursive=recursive_binary_search(nums,target,0,len(nums)-1)
+print("Element found at index(using recursion):",result_recursive)
--- __pycache__/Binary_search.py
+++
@@ -1,6 +0,0 @@
-def binary_search(nums,target):
-    left=0
-    nums.sort() 
-    n=len(nums)
-    right=n-1
-    while l
